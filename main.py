be3f048475d90370ffa17c728939ef7cb590c4fb
brestyck <borissmazz@gmail.com>
2021-05-22 18:37:27 +0300

    Тестовая регистрация

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
+bot = telebot.TeleBot("1850356887:AAEN6-Ykk2T3JSimeBFEkVJwp5wPqDGhDkk")
+name = ''
+surname = ''
+age = 0
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Добро пожаловать в Ереви!\nНапиши /reg для регистрации своего профиля')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_name(message): #получаем фамилию
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
 
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#TEST
-print("TEST")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_age(message):
+    global age
+    while age == 0: #проверяем что возраст изменился
+        try:
+             age = int(message.text) #проверяем, что возраст введен корректно
+        except Exception:
+             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    keyboard = types.InlineKeyboardMarkup() #наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes') #кнопка «Да»
+    keyboard.add(key_yes) #добавляем кнопку в клавиатуру
+    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )')
+    elif call.data == "no":
+         #переспрашиваем
+        bot.send_message(call.message.chat.id, "OK NO")
+bot.polling(none_stop=True, interval=0)
